615984d88e53e55665abbe83ba9f4473e8ce3f4e
Vergel Macayan <94885341+iamvergel@users.noreply.github.com>
2025-10-21 22:35:53 +0800

    10/21/2025

--- src/app/teacher/dashboard/page.tsx
+++ src/app/teacher/dashboard/page.tsx
@@ -1,12 +1,342 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/teacher-sidebar";
+import { Navbar } from "@/components/navbar";
+
+import { SquareArrowOutUpRight, Users, Sheet } from "lucide-react";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
+//Table
+import { StudentTable } from "@/components/admin/dashboard/dashboard-student-table";
+import { TeacherTable } from "@/components/admin/dashboard/dashboard-teacher-table";
+
+//Chart
+import { ChartAreaInteractive } from "@/components/admin/dashboard/student-chart";
+import { TeacherChart } from "@/components/admin/dashboard/teacher-chart";
+
+import { Footer } from "@/components/footer";
+
+import { useAuth } from "@/context/AuthContext";
+// @ts-ignore
+import { useUserDetails } from "@/hooks/useUserDetails";
+import { useClientOnly } from "@/hooks/useClientOnly";
+
+import Loader from "@/components/Loader";
+import { useCountActiveStudents } from "@/hooks/useStudentDetails";
+import { useTeacherActiveCount } from "@/hooks/useTeacher";
+
 export default function Dashboard() {
+  // const { token } = useAuth();
+  // const isClient = useClientOnly();
+
+  // const { data: userDetails, isLoading: isLoadingUserDetails } = useUserDetails(
+  //   token as string
+  // );
+  // const { data: countActiveStudents, isLoading: isLoadingCountActiveStudents } =
+  //   useCountActiveStudents(token as string);
+  // const { data: CountActiveTeachers, isLoading: isLoadingCountActiveTeachers } =
+  //   useTeacherActiveCount(token as string);
+  // console.log("Active Students Count:", countActiveStudents);
+  // if (!isClient || isLoadingUserDetails) {
+  //   return <Loader />;
+  // }
+
   return (
-    
     <ProtectedRoute role="2">
-      <div className="p-3">
-        <h1 className="text-xl">Teacher Dashboard</h1>
-      </div>
+      <SidebarProvider style={{ height: "100vh", width: "100%" }}>
+        <AppSidebar />
+        <main className="w-full h-auto">
+          <Navbar />
+
+          <div className="p-5">
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl">Dashboard</h1>
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Dashboard</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+
+            <div className="mt-10 p-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+              <Card className="border border-yellow-200 hover:border-yellow-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center">
+                    <Users className="h-5 w-5 text-yellow-500 mr-2" />
+                    Active Students
+                  </CardDescription>
+
+                  {/* <CardTitle className="text-4xl"> {countActiveStudents?.active_students_count}</CardTitle> */}
+
+                  <CardAction className="text-white bg-blue-400 px-2 py-1 rounded-full hover:bg-blue-500 text-xs">
+                    <a href="#" className="flex items-center">
+                      Go to Page{" "}
+                      <SquareArrowOutUpRight className="h-3 w-3 ml-2" />
+                    </a>
+                  </CardAction>
+                </CardHeader>
+              </Card>
+              <Card className="border border-teal-200 hover:border-teal-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center">
+                    <Users className="h-5 w-5 text-teal-500 mr-2" />
+                    Active Teachers
+                  </CardDescription>
+
+                  {/* <CardTitle className="text-4xl">{CountActiveTeachers?.count}</CardTitle> */}
+
+                  <CardAction className="text-white bg-blue-400 px-2 py-1 rounded-full hover:bg-blue-500 text-xs">
+                    <a href="#" className="flex items-center">
+                      Go to Page{" "}
+                      <SquareArrowOutUpRight className="h-3 w-3 ml-2" />
+                    </a>
+                  </CardAction>
+                </CardHeader>
+              </Card>
+              <Card className="border border-blue-200 hover:border-blue-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center">
+                    <Users className="h-5 w-5 text-blue-500 mr-2" />
+                    Active Employees
+                  </CardDescription>
+
+                  <CardTitle className="text-4xl">0</CardTitle>
+
+                  <CardAction className="text-white bg-blue-400 px-2 py-1 rounded-full hover:bg-blue-500 text-xs">
+                    <a href="#" className="flex items-center">
+                      Go to Page{" "}
+                      <SquareArrowOutUpRight className="h-3 w-3 ml-2" />
+                    </a>
+                  </CardAction>
+                </CardHeader>
+              </Card>
+            </div>
+
+            <div>
+              <h1 className="text-xl mt-10 flex">
+                <Users className="mr-2 w-7 h-7" />
+                Totol Student Per Grades
+              </h1>
+            </div>
+
+            <div className="mt-10 p-3 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-5">
+              <Card className="bg-green-200 hover:bg-green-300 text-black dark:text-white dark:bg-green-400 dark:hover:bg-green-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center text-black dark:text-white ">
+                    <Users className="h-5 w-5 mr-2" />
+                    Grade One
+                  </CardDescription>
+
+                  <CardTitle className="text-4xl">0</CardTitle>
+                </CardHeader>
+              </Card>
+
+              <Card className="bg-orange-200 hover:bg-orange-300 text-black dark:text-white dark:bg-orange-400 dark:hover:bg-orange-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center text-black dark:text-white ">
+                    <Users className="h-5 w-5 mr-2" />
+                    Grade Two
+                  </CardDescription>
+
+                  <CardTitle className="text-4xl">0</CardTitle>
+                </CardHeader>
+              </Card>
+
+              <Card className="bg-indigo-200 hover:bg-indigo-300 text-black dark:text-white dark:bg-indigo-400 dark:hover:bg-indigo-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center text-black dark:text-white ">
+                    <Users className="h-5 w-5 mr-2" />
+                    Grade Three
+                  </CardDescription>
+
+                  <CardTitle className="text-4xl">0</CardTitle>
+                </CardHeader>
+              </Card>
+
+              <Card className="bg-blue-200 hover:bg-blue-300 text-black dark:text-white dark:bg-blue-400 dark:hover:bg-blue-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center text-black dark:text-white ">
+                    <Users className="h-5 w-5 mr-2" />
+                    Grade Four
+                  </CardDescription>
+
+                  <CardTitle className="text-4xl">0</CardTitle>
+                </CardHeader>
+              </Card>
+
+              <Card className="bg-red-200 hover:bg-red-300 text-black dark:text-white dark:bg-red-400 dark:hover:bg-red-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center text-black dark:text-white ">
+                    <Users className="h-5 w-5 mr-2" />
+                    Grade Five
+                  </CardDescription>
+
+                  <CardTitle className="text-4xl">0</CardTitle>
+                </CardHeader>
+              </Card>
+
+              <Card className="bg-yellow-200 hover:bg-yellow-300 text-black dark:text-white dark:bg-yellow-400 dark:hover:bg-yellow-500">
+                <CardHeader>
+                  <CardDescription className="flex items-center text-black dark:text-white ">
+                    <Users className="h-5 w-5 mr-2" />
+                    Grade Six
+                  </CardDescription>
+
+                  <CardTitle className="text-4xl">0</CardTitle>
+                </CardHeader>
+              </Card>
+            </div>
+
+            <div className="my-5 px-5">
+              <ChartAreaInteractive />
+            </div>
+
+            <div>
+              <h1 className="text-xl mt-10 flex">
+                <Sheet className="mr-2 w-7 h-7" />
+                Student Attendance Today
+              </h1>
+            </div>
+
+            <div className="mt-5 p-3 grid grid-cols-1 md:grid-cols-3 gap-10 w-full">
+              <div className="col-span-1 md:col-span-2">
+                <StudentTable />
+              </div>
+              <div className="col-span-1 grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+                <Card className="bg-green-200 hover:bg-green-300 text-black dark:text-white dark:bg-green-400 dark:hover:bg-green-500">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      Present
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+
+                <Card className="bg-yellow-200 hover:bg-yellow-300 text-black dark:text-white dark:bg-yellow-400 dark:hover:bg-yellow-500">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      Late
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+
+                <Card className="bg-red-200 hover:bg-red-300 text-black dark:text-white dark:bg-red-400 dark:hover:bg-red-500">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      Absent
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+
+                <Card className="bg-gray-200 text-black dark:text-white dark:bg-gray-400">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      ---
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+              </div>
+            </div>
+
+            <div className="my-5 px-5">
+              <TeacherChart />
+            </div>
+
+            <div>
+              <h1 className="text-xl mt-10 flex ">
+                <Sheet className="mr-2 w-7 h-7" />
+                Teacher Attendance Today
+              </h1>
+            </div>
+
+            <div className="mt-5 p-3 grid grid-cols-1 md:grid-cols-3 gap-10 w-full">
+              <div className="col-span-1 grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+                <Card className="bg-green-200 hover:bg-green-300 text-black dark:text-white dark:bg-green-400 dark:hover:bg-green-500">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      Present
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+
+                <Card className="bg-yellow-200 hover:bg-yellow-300 text-black dark:text-white dark:bg-yellow-400 dark:hover:bg-yellow-500">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      Late
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+
+                <Card className="bg-red-200 hover:bg-red-300 text-black dark:text-white dark:bg-red-400 dark:hover:bg-red-500">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      Absent
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+
+                <Card className="bg-gray-200 text-black dark:text-white dark:bg-gray-400">
+                  <CardHeader>
+                    <CardDescription className="flex items-center text-black dark:text-white ">
+                      <Users className="h-5 w-5 mr-2" />
+                      ---
+                    </CardDescription>
+
+                    <CardTitle className="text-4xl">0</CardTitle>
+                  </CardHeader>
+                </Card>
+              </div>
+              <div className="col-span-1 md:col-span-2">
+                <TeacherTable />
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </main>
+      </SidebarProvider>
     </ProtectedRoute>
   );
 }
---
+++ src/components/teacher-sidebar.tsx
@@ -0,0 +1,141 @@
+"use client";
+import {
+  Calendar,
+  Home,
+  Inbox,
+  SquareUser,
+  Users,
+  IdCardLanyard,
+  Settings,
+  ShieldUser,
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+  SidebarGroupLabel,
+} from "@/components/ui/sidebar";
+import { useAuth } from "@/context/AuthContext";
+// @ts-ignore
+import { useUserDetails } from "@/hooks/useUserDetails";
+import { useEffect, useState } from "react";
+import { useClientOnly } from "@/hooks/useClientOnly";
+
+import SplitTextSide from "@/components/animata/text/split-text-side";
+
+const items = [
+  { title: "Dashboard", url: "/admin/dashboard", icon: Home },
+  { title: "Email", url: "/admin/email", icon: Inbox },
+  { title: "Calendar", url: "/admin/calendar", icon: Calendar },
+  { title: "Manage Student", url: "/admin/manage-student", icon: SquareUser },
+  {
+    title: "Manage Teacher",
+    url: "/admin/manage-teacher",
+    icon: IdCardLanyard,
+  },
+  { title: "Manage Admin", url: "/admin/manage-admin", icon: ShieldUser },
+  { title: "System Settings", url: "/admin/system-settings", icon: Settings },
+];
+
+export function AppSidebar() {
+  const pathname = usePathname();
+
+  const { token } = useAuth();
+  const isClient = useClientOnly();
+
+  const { data: userDetails, isLoading: isLoadingUserDetails } = useUserDetails(
+    token as string
+  );
+
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        <SidebarGroup style={{ pointerEvents: "auto" }}>
+          <div className="flex flex-col items-center justify-center gap-5 p-3 group-data-[collapsible=icon]:mt-3 group-data-[collapsible=icon]:mb-3 group-data-[collapsible=icon]:gap-0 group-data-[collapsible=icon]:p-0">
+            <img
+              src="/logo.png"
+              alt="logo"
+              className="w-25 h-25 group-data-[collapsible=icon]:block group-data-[collapsible=icon]:w-8 group-data-[collapsible=icon]:h-8"
+            />
+
+            <SplitTextSide
+              text="Young Generation Academy"
+              className="group-data-[collapsible=icon]:hidden"
+            />
+          </div>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu className="mt-2">
+              {items.map(({ title, url, icon: Icon }) => (
+                <SidebarMenuItem key={title}>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <SidebarMenuButton
+                        asChild
+                        className={`h-10 group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:items-center group-data-[collapsible=icon]:gap-2 ${
+                          url === pathname
+                            ? "bg-teal-600 h-10 text-white hover:bg-teal-800 hover:text-white"
+                            : ""
+                        }`}
+                      >
+                        <a href={url}>
+                          <Icon className="size-4" />
+                          <span className="block group-data-[collapsible=icon]:hidden">
+                            {title}
+                          </span>
+                        </a>
+                      </SidebarMenuButton>
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="right"
+                      align="center"
+                      className="block group-data-[collapsible=icon]:hidden"
+                    >
+                      {title}
+                    </TooltipContent>
+                  </Tooltip>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter className="py-5">
+        <div className="flex items-center gap-2">
+          <Avatar className="h-8 w-8 flex-shrink-0">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          <div className="px-2 py-2 block group-data-[collapsible=icon]:hidden">
+            <p className="font-semibold text-xs">
+              {userDetails?.data.first_name + " " + userDetails?.data.last_name}
+              <span className="text-[10px] px-2 py-0 bg-teal-600 text-white rounded-full">
+                {userDetails?.data.role_id === 1
+                  ? "Admin"
+                  : userDetails?.data.role_id === 2
+                  ? "Teacher"
+                  : ""}
+              </span>
+            </p>
+            <p className="text-xs">{userDetails?.data.email}</p>
+          </div>
+        </div>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
